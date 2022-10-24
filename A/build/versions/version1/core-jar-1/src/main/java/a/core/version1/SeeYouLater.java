6e05bf097aea74af5dcbe5aa639fdf40b3a23c77
Jeff Thomas <jeffrey.thomas@t-systems.com>
2022-10-24 13:22:18 +0200

    IDEA-304403 - Attempt to fix with build-helper-maven-plugin - FAILED

---
+++ A/build/versions/version1/core-jar-1/src/main/java/a/core/version1/SeeYouLater.java
@@ -0,0 +1,15 @@
+package a.core.version1;
+
+import a.core.HelloThere;
+
+public class SeeYouLater {
+
+  public String sayHallo() {
+    return (new HelloThere()).sayHello();
+  }
+
+  public String saySeeYouLater() {
+    return "See You Later, Alligator!";
+  }
+
+}
---
+++ A/build/versions/version2/core-jar-2/src/main/java/a.core/version2/InAWhile.java
@@ -0,0 +1,13 @@
+package a.core.version1;
+
+public class SeeYouLater {
+
+  public String sayHallo() {
+    return (new HelloThere()).sayHello();
+  }
+
+  public String sayInAWhile() {
+    return "In A While, Crocodile!";
+  }
+
+}
